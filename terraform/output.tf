589c74e540ce1128a1b482a6b23d283b70368688
AVIRAL KHANDELWAL <aviral556@gmail.com>
2025-07-16 11:20:09 +0530

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,10 @@
+output "cluster_endpoint" {
+ value = module.eks.cluster_endpoint
+}
+output "cluster_name" {
+ value = module.eks.cluster_name
+}
+output "kubeconfig" {
+ value = module.eks.kubeconfig
+ sensitive = true
+}
